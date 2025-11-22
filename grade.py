b5463f22e9e503ebba8a4a36cba2bb23069622b6
shamith777 <142828883+shamith777@users.noreply.github.com>
2025-11-22 12:09:13 +0530

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,17 +1,24 @@
-marks =[]
-for i in range(1,6):
-    m = float(input(f"Enter marks for subject {i}: "))
-    marks.append(m)
+import sys
+if len(sys.argv) != 6:
+    print("ERROR: Please provide 5 subject marks as parameters.")
+    print("Usage: python3 marks.py <SUB1> <SUB2> <SUB3> <SUB4> <SUB5>")
+    sys.exit(1)
+marks = list(map(float, sys.argv[1:6]))
+print("\n=== INPUT RECEIVED FROM JENKINS PARAMETERS ===")
+print("Marks:", marks)
 avg = sum(marks) / 5
-if(avg >= 90):
+vg_marks = [m for m in marks if m > 90]
+if avg >= 90:
     grade = "A"
-elif(avg >= 75):
+elif avg >= 75:
     grade = "B"
-elif(avg >= 60):
-    grade = "C" 
-elif(avg >= 50):    
+elif avg >= 60:
+    grade = "C"
+elif avg >= 40:
     grade = "D"
 else:
     grade = "Fail"
-
-    
+print("\n===== RESULT =====")
+print("Average:", avg)
+print("Very Good Marks (>90):", vg_marks if vg_marks else "None")
+print("Grade:", grade)
