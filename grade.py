34de7e234fc0d8498068045d2b930023d3c9db5f
shamith777 <142828883+shamith777@users.noreply.github.com>
2025-11-22 12:11:02 +0530

    Refactor grade.py to simplify output
    Removed unnecessary print statements and adjusted output formatting.

--- grade.py
+++ grade.py
@@ -4,10 +4,8 @@ if len(sys.argv) != 6:
     print("Usage: python3 marks.py <SUB1> <SUB2> <SUB3> <SUB4> <SUB5>")
     sys.exit(1)
 marks = list(map(float, sys.argv[1:6]))
-print("\n=== INPUT RECEIVED FROM JENKINS PARAMETERS ===")
 print("Marks:", marks)
 avg = sum(marks) / 5
-vg_marks = [m for m in marks if m > 90]
 if avg >= 90:
     grade = "A"
 elif avg >= 75:
@@ -18,7 +16,6 @@ elif avg >= 40:
     grade = "D"
 else:
     grade = "Fail"
-print("\n===== RESULT =====")
+
 print("Average:", avg)
-print("Very Good Marks (>90):", vg_marks if vg_marks else "None")
 print("Grade:", grade)
